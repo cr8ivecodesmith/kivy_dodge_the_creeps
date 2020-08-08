1f191c50e3007226dcfc4ff6555eb0ce7a2ebe74
Matt Lebrun <matt@lebrun.org>
2020-08-08 20:47:45 +0800

    Initial implementation of collision and path systems

---
+++ common/collision.py
@@ -0,0 +1,55 @@
+"""
+Collision Detection System
+
+General checking process:
+
+- Pick target A
+- Get all collision coords from the target A's collsion shape (NOT IMPLEMENTED)
+- Pick target B
+- Get all collision coords from the target B's collsion shape (NOT IMPLEMENTED)
+- If any of their points are within the collision shape's area, trigger
+  their `on_body_entered` event
+- Remove both of them from the
+
+"""  #noqa
+from itertools import combinations
+
+from kivy.logger import Logger as log
+from kivy.uix.widget import Widget
+
+
+class CollisionSystem(Widget):
+
+    targets = set()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # FPS Offset Timers
+        self._time = .0
+        self._rate = 1 / 60
+
+    def register(self, target):
+        self.targets.add(target)
+
+    def unregister(self, target):
+        self.targets.remove(target)
+
+    def process(self, delta):
+        self._time += delta
+        if self._time > self._rate:
+            self._time -= self._rate
+
+            for a, b in combinations(self.targets, 2):
+                # Ignore invalid comparisons
+                if not a.visible or not b.visible:
+                    continue
+                if set(a.neutral_groups) & set(b.neutral_groups):
+                    continue
+
+                # TODO: Get target collision shapes if it exists
+                if a.collide_widget(b):
+                    a.dispatch('on_body_entered', b)
+                    b.dispatch('on_body_entered', a)
+        else:
+            log.debug(f'COLISSION CHECKS: SKIPPED @ DELTA {self._time}')
---
+++ common/path2d.py
@@ -0,0 +1,46 @@
+from kivy.logger import Logger as log
+from kivy.uix.widget import Widget
+from kivy.vector import Vector
+
+
+class Path2D(Widget):
+
+    spawn_location = None
+    targets = set()
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        # FPS Timer Offset
+        self._time = .0
+        self._rate = 1 / 60
+
+    def register(self, target):
+        self.targets.add(target)
+
+    def unregister(self, target):
+        self.targets.remove(target)
+
+    def process(self, delta):
+        # TODO: Update targets position
+        # targets must have a linear_velocity and direction properties
+        self._time += delta
+        if self._time > self._rate:
+            self._time -= self._rate
+
+            window = self.get_root_window()
+            w, h = window.size
+
+            for target in self.targets:
+                if not target.visible:
+                    continue
+
+                x, y = target.center
+                if (x < 0 or x > w) or (y < 0 or y > h):
+                    # TODO: Trigger removal / hide event
+                    target.visible = False
+                    continue
+                spd = Vector(target.speed, target.speed) * delta
+                target.pos = Vector(*target.pos) + spd
+        else:
+            log.debug(f'PATH MOVEMENT: SKIPPED @ DELTA {self._time}')
