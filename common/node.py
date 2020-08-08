c82c94fb13a9004dd72647cee5969b7917ab1c0a
Matt Lebrun <matt@lebrun.org>
2020-08-08 20:42:55 +0800

    Renamed module

---
+++ common/node.py
@@ -0,0 +1,33 @@
+"""
+Base Entity Node
+
+"""
+from kivy.properties import AliasProperty, BooleanProperty
+from kivy.uix.widget import Widget
+
+
+class Node(Widget):
+
+    visible = BooleanProperty(True)
+
+    def _set_ng(self, val): self._neutral_groups = set(val)
+
+    def _get_ng(self): return self._neutral_groups
+
+    neutral_groups = AliasProperty(_get_ng, _set_ng)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.register_event_type('on_body_entered')
+        self.bind(
+            visible=self._handle_visible,
+        )
+        self._neutral_groups = set()
+
+    def on_body_entered(self, other):
+        pass
+
+    def _handle_visible(self, this, value):
+        self.size_hint_x = 1 if value else 0
+        self.opacity = 1 if value else 0
+        self.disabled = not value
